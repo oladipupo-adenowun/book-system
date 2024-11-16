b7ba3f6533501e84ca2672f54a8e0893f0389c89
Oladipupo Adenowun <oladipupo.adenowun@yahoo.com>
2024-11-15 19:48:27 -0500

    fixed undefined error in editbook

--- script.js
+++ script.js
@@ -28,6 +28,8 @@ function addBook(){
             <p><strong>Author Name: </strong>${book.authorName}</p>
             <p><strong>Book Description: </strong>${book.bookDescription}</p>
             <p><strong>No. of Pages: </strong>${book.pagesNumber}</p>
+            <p><button id="edit_book" onclick="editBook(${index+1})">Edit Book</button>
+        <button id="delete_book" onclick="deleteBook(${index+1})">Delete Book</button></p>
             `
         ).join('');
         document.getElementById('books').innerHTML = booksHTML;
@@ -43,4 +45,40 @@ function addBook(){
     function deleteBook(bookId){
         books.splice(bookId-1,1);
         showBooks();
+    }
+
+    function editBook(bookId){
+        const index = bookId-1;
+        const editBook = books[index];
+        document.getElementById("bookName").value = editBook.name;
+        document.getElementById("authorName").value = editBook.authorName;
+        document.getElementById("bookDescription").value = editBook.bookDescription;
+        document.getElementById("pagesNumber").value = editBook.pagesNumber;
+
+        const addButton = document.getElementById("add_book");
+        addButton.innerText = "Update";
+        addButton.onclick = ()=>{
+            updateBook(index);
+        }
+    }
+
+    function updateBook(bookId){
+        const bookName = document.getElementById('bookName').value;
+        const authorName = document.getElementById('authorName').value;
+        const bookDescription = document.getElementById('bookDescription').value;
+        const pagesNumber = document.getElementById('pagesNumber').value;
+    
+        if(bookName && authorName && bookDescription && !isNaN(pagesNumber)){
+            const book = {
+                name: bookName,
+                authorName: authorName,
+                bookDescription: bookDescription,
+                pagesNumber: pagesNumber
+            };
+            books.splice(bookId,1,book);
+            showBooks();
+            clearInputs();
+        }else{
+            alert('Please fill all fields correctly!');
+        }
     }
